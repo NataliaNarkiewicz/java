bf837ab12ec8aa642a9ace1f0c224779a7dc4ff4
NataliaNarkiewicz <natalianarkiewicz7@gmail.com>
2025-06-13 11:48:30 +0200

    Update narkiewicz_zewnetrzny.js

--- narkiewicz_zewnetrzny.js
+++ narkiewicz_zewnetrzny.js
@@ -1,6 +1,6 @@
 document.write(
   "ostatnia modyfikacja strony"
-    .fontcolor("#0000B0")   // kolor 11
+    .fontcolor("808000")   // kolor 11
     .bold()
     .strike()
     .fontsize(5) + "<br>"
